6f58e72374c08304ce2cf612c7c9d928ee9a57f2
stewartqqq <168217578+stewartqqq@users.noreply.github.com>
2024-11-22 23:33:00 +0500

    somtheing another

--- index.js
+++ index.js
@@ -4,3 +4,13 @@ window.onload = function () {
   console.log(top);
   window.scrollTo(0, top);
 };
+var viewAllButton = false
+document.getElementById("MS-Button").onclick = function(){
+  if(!viewAllButton){
+document.getElementById("ML-Container").style.display = "flex";
+  viewAllButton = true
+  }else{
+    document.getElementById("ML-Container").style.display = "none";
+  viewAllButton = false
+  }
+}
